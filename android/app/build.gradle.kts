6c1fec7c9ddb019312911d2ec35274abb5796282
ikegami <ikegami@example.com>
2026-02-24 20:19:57 +0900

    feat: prepare for Android release by updating package name to jp.ikegam1.simple_molkky_score (v0.3.0)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.simple_molkky_score"
+    namespace = "jp.ikegam1.simple_molkky_score"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.simple_molkky_score"
+        applicationId = "jp.ikegam1.simple_molkky_score"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
