610035f4c9ea8da6088bfe506a52fc6ee999002a
ikegam1 <ikegam1@users.noreply.github.com>
2026-03-25 02:27:52 +0000

    deploy: a3f3c1f16c0d59862691f49a235e25acd658191d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3290333422" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2938625291" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
