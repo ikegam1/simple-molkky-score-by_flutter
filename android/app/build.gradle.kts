b042c748143020b4d78adac875da254f464d1174
ikegami <ikegami@example.com>
2026-03-25 10:30:28 +0900

    bump: version 1.9.0+41 and update Android NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "jp.ikegam1.simple_molkky_score"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
